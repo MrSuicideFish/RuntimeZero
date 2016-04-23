d0a192ffffcf718bc2c0cb38dbe543d2c1ec8073
Malcolm Neal <Malcolm Neal>
2016-04-23 16:04:00 -0400

    added 16 bit shader

---
+++ RuntimeZero/Assets/Scripts/Image FX/LowBitImageFX.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LowBitImageFX : MonoBehaviour
+{
+    private Material FXMaterial;
+
+    void OnRenderImage( RenderTexture source, RenderTexture destination )
+    {
+        if (!FXMaterial)
+        {
+            FXMaterial = new Material( Shader.Find( "Shader Forge/16BitImage" ) );
+        }
+
+        //mat is the material containing your shader
+        Graphics.Blit( source, destination, FXMaterial );
+    }
+}
