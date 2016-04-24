7feedfc63b6329696be6294c5fcfb905addc9fc0
GarisonHayne <Garison@live.ca>
2016-04-24 02:22:37 -0300

    Ai scripts Start: Simple follow

---
+++ RuntimeZero/Assets/Scripts/Ai/MoveTo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour
+{
+
+    public Transform goal;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+       agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    void Update()
+    {
+        agent.destination = goal.position;
+    }
+}
